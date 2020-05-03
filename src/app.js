4e1e407fbf13abdcbedb0467692d69578a6a0f3d
Mark Adkins <mark@bymark.co>
2020-05-03 02:40:27 -0400

    Revert "Revert "Initial Configuration""
    This reverts commit 1c18dfa47046e36bfac39f0e5f5b25e80e86ed24.

--- src/app.js
+++ src/app.js
@@ -4,25 +4,40 @@ let passthrough = process.env.PASSTHROUGH;
 let slug = window.location.pathname;
 
 // Use console mode to prevent redirects during development.
-let consoleMode = false;
+let consoleMode = true;
 
 let redirected = false;
+
+let destination = "";
+
+function setDestination(value){
+    destination = value;
+    document.getElementById("redirect-location").innerHTML = destination;
+    document.getElementById("redirect-button").href = destination;
+}
+
+function goNow() {
+    console.log("Redirect triggered");
+    window.location.replace(destination);
+}
 
 for(let i = 0; i < redirects.length; i++) {
     if(redirects[i].slug === slug || redirects[i].slug === slug  + "/") {
         redirected = true;
+        setDestination(redirects[i].destination);
         console.log("redirect: ", true);
         console.log(slug);
         if(!consoleMode) {
-            window.location.replace(redirects[i].destination);
+            goNow();
         }
     }
 }
 
 if(!redirected) {
     if(!consoleMode) {
-        window.location.replace(passthrough + slug);
+        goNow();
     }
+    setDestination(passthrough + slug);
     console.log("redirect: ", "passthrough");
     console.log(passthrough);
 }
