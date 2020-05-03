5de8d779ae78ca983e8038db35ddfbfd846dc91b
Mark Adkins <mark@bymark.co>
2020-05-03 01:38:22 -0400

    Added Console Mode

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import redirects from './redirects';
 let passthrough = process.env.PASSTHROUGH;
 let slug = window.location.pathname;
 
+// Use console mode to prevent redirects during development.
+let consoleMode = false;
+
 let redirected = false;
 
 for(let i = 0; i < redirects.length; i++) {
@@ -10,12 +13,16 @@ for(let i = 0; i < redirects.length; i++) {
         redirected = true;
         console.log("redirect: ", true);
         console.log(slug);
-        window.location.replace(redirects[i].destination);
+        if(!consoleMode) {
+            window.location.replace(redirects[i].destination);
+        }
     }
 }
 
 if(!redirected) {
-    window.location.replace(passthrough + slug);
+    if(!consoleMode) {
+        window.location.replace(passthrough + slug);
+    }
     console.log("redirect: ", "passthrough");
     console.log(passthrough);
 }
